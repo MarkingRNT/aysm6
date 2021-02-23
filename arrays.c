620861b3b3332230ea7dbc0e37231e5fc889bd0e
Fabrizio <fabrizio.carlassara@gmail.com>
2021-02-23 00:09:14 -0300

    Initial commit

---
+++ arrays.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+    // Verifico que se haya ejecutado el programa indicando la cantidad de elementos
+    if (argc < 2) {
+        printf("Uso: ./arrays [cantidad de elementos]");
+        return 0;
+    }
+    int n = atoi(argv[1]);  // Convierto el caracter a un numero entero
+    int nums[n];            // Declaro un array con n cantidad de elementos
+
+    // TODO
+
+    return 0;
+} 
