58b81c40de09a582b48afa06e62bcaf52580dc7b
Fabrizio <fabrizio.carlassara@gmail.com>
2021-02-23 00:15:55 -0300

    Initial commit

---
+++ pointers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+    // Verifico que el programa se ejecute indicando la cadena
+    if (argc < 2) {
+        printf("Uso: ./pointers [cadena de caracteres]");
+        return 0;
+    }
+
+    // TODO
+
+    return 0;
+}
