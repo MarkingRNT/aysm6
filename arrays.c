241d1fd729caaeb3737f58f388635e96c00dd8bd
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2021-04-08 09:21:57 -0300

    Update arrays.c

--- arrays.c
+++ arrays.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[]) {
     // Verifico que se haya ejecutado el programa indicando la cantidad de elementos
     if (argc < 2) {
         printf("Uso: ./arrays [cantidad de elementos]");
-        return 0;
+        return 1;
     }
     int n = atoi(argv[1]);  // Convierto el caracter a un numero entero
     int nums[n];            // Declaro un array con n cantidad de elementos
